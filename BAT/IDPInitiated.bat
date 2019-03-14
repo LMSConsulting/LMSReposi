fcc11bfd8e003b8ebfdcaaf39767d9b2a2d2c181
LMSConsulting <support@likemindsconsulting.com>
2019-03-14 06:44:45 +0000

    removing folder

--- BAT/IDPInitiated.bat
+++
@@ -1,4 +0,0 @@
-set projectLocation=C:\Users\Administrator\eclipse-workspace1\SeleniumAutomation
-cd %projectLocation%
-set classpath=%projectLocation%\bin;%projectLocation%\lib\*
-java org.testng.TestNG %projectLocation%\IDPInintiated.xml
