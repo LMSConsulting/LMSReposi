d1431880ae49cfafa98ce36d71f6c067a3858213
LMSConsulting <support@likemindsconsulting.com>
2019-03-14 06:43:40 +0000

    removing

--- FormBased_Internal.bat
+++
@@ -1,4 +0,0 @@
-set projectLocation=C:\Users\Administrator\eclipse-workspace\Selenium1
-cd %projectLocation%
-set classpath=%projectLocation%\bin;%projectLocation%\lib\*
-java org.testng.TestNG %projectLocation%\FormBased_Internal.xml
--- OAuth.bat
+++
@@ -1,4 +0,0 @@
-set projectLocation=C:\Users\Administrator\eclipse-workspace\Selenium1
-cd %projectLocation%
-set classpath=%projectLocation%\bin;%projectLocation%\lib\*
-java org.testng.TestNG %projectLocation%\OAuth.xml
