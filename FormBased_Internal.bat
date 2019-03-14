aed9302101c0a6a9421cdce615ee5565bde720bc
LMSConsulting <support@likemindsconsulting.com>
2019-03-14 06:45:33 +0000

    allfiles

---
+++ FormBased_Internal.bat
@@ -0,0 +1,4 @@
+set projectLocation=C:\Users\Administrator\eclipse-workspace1\SeleniumAutomation
+cd %projectLocation%
+set classpath=%projectLocation%\bin;%projectLocation%\lib\*
+java org.testng.TestNG %projectLocation%\FormBased_internal.xml
